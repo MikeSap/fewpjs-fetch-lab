475c4013f151f5b72040655a94aa6dea01ccdf9d
mike <mikesapienzaaudio@gmail.com>
2020-10-28 10:32:17 -0500

    commit tree eb43c6deb1273acea8120c509193934acd040239

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function fetchBooks() {
     // 1031st character in story. Make map of json.book.characters? point to array index 1030
     // total pages of all books. map json.book.pages saved to variable then pages.length
     books = json
-    renderBooks(json)
+    renderBooks(books)
   });
 }
 
